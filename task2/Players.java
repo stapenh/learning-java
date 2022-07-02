f212fca7a0f128e8cd67409722ee64a174772b20
anfisamulina <mulin.stepan2013@yandex.ru>
2022-07-02 12:44:04 +0300

    help me please fix it

--- task2/Players.java
+++ task2/Players.java
@@ -1,11 +1,11 @@
 public class Players{
     String[] a={"камень","ножницы","бумага"};
-    int b=a.length;
     String ruka="ladonb";
     public static void main(String[] args) {
         System.out.print("main zaebal");
     }
     public  void hand(){
+        int b=a.length;
          ruka=a[(int)(Math.random()*b)];
         System.out.println(ruka);
 
